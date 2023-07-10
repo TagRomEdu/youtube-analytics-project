de908c6c094971b4a60ac2f0fe3c6e2f3c61a810
TagRomEdu <qwertystyles@gmail.com>
2023-07-10 13:27:43 +0500

    added class method for return API youtube object

--- src/channel.py
+++ src/channel.py
@@ -6,15 +6,27 @@ API_YOUTUBE = os.getenv('API_YOUTUBE')
 youtube = build("youtube", "v3", developerKey=API_YOUTUBE)
 
 
+
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         request = youtube.channels().list(part="snippet,contentDetails", id=self.channel_id)
         response = request.execute()
         print(response)
+
+    @classmethod
+    def get_service(cls):
+        """
+        Возвращает объект для работы с YouTube API
+        """
+        return youtube
