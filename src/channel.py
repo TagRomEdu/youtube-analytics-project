4dcd77ea6b5e86c5eb1aa4de902ae3436bfb8d25
TagRomEdu <qwertystyles@gmail.com>
2023-07-10 15:57:44 +0500

    fixed methods: added statistics from channel; added attributes for exemplars

--- src/channel.py
+++ src/channel.py
@@ -8,19 +8,21 @@ API_YOUTUBE = os.getenv('API_YOUTUBE')
 youtube = build("youtube", "v3", developerKey=API_YOUTUBE)
 
 
-
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        request = youtube.channels().list(part="snippet,contentDetails,statistics", id=channel_id)
+        response = request.execute()
+
         self.__channel_id = channel_id
-        self.title = ''
-        self.description = ''
-        self.url = ''
-        self.subscriber_count = 0
-        self.view_count = 0
-        self.video_count = 0
+        self.title = response["items"][0]["snippet"]["title"]
+        self.description = response["items"][0]["snippet"]["description"]
+        self.url = f'https://www.youtube.com/channel/{channel_id}'
+        self.subscriber_count = response["items"][0]["statistics"]["subscriberCount"]
+        self.view_count = response["items"][0]["statistics"]["viewCount"]
+        self.video_count = response["items"][0]["statistics"]["videoCount"]
 
     @property
     def channel_id(self):
@@ -28,7 +30,7 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        request = youtube.channels().list(part="snippet,contentDetails", id=self.channel_id)
+        request = youtube.channels().list(part="snippet,contentDetails,statistics", id=self.channel_id)
         response = request.execute()
         print(response)
 
@@ -43,9 +45,8 @@ class Channel:
         """
         Сохраняет данные в JSON-файл
         """
-        request = youtube.channels().list(part="snippet,contentDetails", id=self.channel_id)
+        request = youtube.channels().list(part="snippet,contentDetails,statistics", id=self.channel_id)
         response = request.execute()
 
         with open(file_name, 'w') as file:
             json.dump([response], file)
-
