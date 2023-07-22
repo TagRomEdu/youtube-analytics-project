2cb31e6af6ba39881e6367828623528fb4d08fc8
TagRomEdu <qwertystyles@gmail.com>
2023-07-22 22:05:01 +0500

    added class PLVideo(Video)

--- src/video.py
+++ src/video.py
@@ -2,7 +2,9 @@ from src.channel import Channel
 
 
 class Video(Channel):
+
     def __init__(self, video_id):
+        """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
         request = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id)
         self.response = request.execute()
 
@@ -13,6 +15,7 @@ class Video(Channel):
         self.view_count = self.response["items"][0]["statistics"]["viewCount"]
 
 
-
-video1 = Video('AWX4JnAnjBE')
-print(video1.title)
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
