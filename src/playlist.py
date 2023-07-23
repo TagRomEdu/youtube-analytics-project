1bd44e1e965d8be08695c56ff08dd83720950900
TagRomEdu <qwertystyles@gmail.com>
2023-07-23 20:25:50 +0500

    added total duration method

--- src/playlist.py
+++ src/playlist.py
@@ -1,3 +1,6 @@
+import isodate as isodate
+import datetime
+
 from src.channel import Channel
 
 
@@ -6,13 +9,31 @@ class PlayList(Channel):
         """
         Экземпляр инициализируется id плейлиста. Дальше все данные будут подтягиваться по API.
         """
-        request = self.get_service().playlistItems().list(playlistId=playlist_id,
-                                                          part='contentDetails, snippet',
-                                                          maxResults=50,
-                                                          )
+        request = self.get_service().playlistItems().list(playlistId=playlist_id, part='contentDetails, snippet')
         self.response = request.execute()
+        snippet = self.get_service().playlists().list(id=playlist_id, part='snippet').execute()
 
         self.playlist_id = playlist_id
-        self.title = self.response["items"][0]["snippet"]["title"]
+        self.title = snippet["items"][0]["snippet"]["title"]
         self.url = f'https://www.youtube.com/playlist?list={playlist_id}'
+        self.video_ids = [video['contentDetails']['videoId'] for video in self.response['items']]
 
+    def total_duration(self):
+        video_response = pl.get_service().videos().list(part='contentDetails,statistics',
+                                                        id=','.join(pl.video_ids)
+                                                        ).execute()
+        delta = datetime.timedelta()
+        for video in video_response['items']:
+            # YouTube video duration is in ISO 8601 format
+            iso_8601_duration = video['contentDetails']['duration']
+            duration = isodate.parse_duration(iso_8601_duration)
+            delta += duration
+
+        return delta
+
+    def show_best_video(self):
+        pass
+
+
+pl = PlayList('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
+print(pl.title)
