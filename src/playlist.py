6af941a35e7b15027cff30bf684f3aa4c8d700ee
TagRomEdu <qwertystyles@gmail.com>
2023-07-23 21:16:54 +0500

    added method for choosing the best video

--- src/playlist.py
+++ src/playlist.py
@@ -2,6 +2,7 @@ import isodate as isodate
 import datetime
 
 from src.channel import Channel
+from src.video import Video
 
 
 class PlayList(Channel):
@@ -19,21 +20,24 @@ class PlayList(Channel):
         self.video_ids = [video['contentDetails']['videoId'] for video in self.response['items']]
 
     def total_duration(self):
-        video_response = pl.get_service().videos().list(part='contentDetails,statistics',
-                                                        id=','.join(pl.video_ids)
-                                                        ).execute()
+        """
+        Метод для определения общей длительности.
+        """
+        video_response = self.get_service().videos().list(part='contentDetails,statistics', bid=','.join(self.video_ids)
+                                                          ).execute()
         delta = datetime.timedelta()
         for video in video_response['items']:
             # YouTube video duration is in ISO 8601 format
             iso_8601_duration = video['contentDetails']['duration']
             duration = isodate.parse_duration(iso_8601_duration)
             delta += duration
-
         return delta
 
     def show_best_video(self):
-        pass
-
-
-pl = PlayList('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
-print(pl.title)
+        """
+        Метод для выявления лучшего из видео по лайкам.
+        """
+        vid_lst = [Video(video) for video in self.video_ids]
+        for v in vid_lst:
+            sorted(v.like_count)
+        return vid_lst[-1].url
