665b99faa20235fbd5dddf7326659d3bf991cd4e
TagRomEdu <qwertystyles@gmail.com>
2023-07-23 18:50:48 +0500

    added initialization with playlist id, added attrs title & url

--- src/playlist.py
+++ src/playlist.py
@@ -2,4 +2,17 @@ from src.channel import Channel
 
 
 class PlayList(Channel):
-    pass
+    def __init__(self, playlist_id):
+        """
+        Экземпляр инициализируется id плейлиста. Дальше все данные будут подтягиваться по API.
+        """
+        request = self.get_service().playlistItems().list(playlistId=playlist_id,
+                                                          part='contentDetails, snippet',
+                                                          maxResults=50,
+                                                          )
+        self.response = request.execute()
+
+        self.playlist_id = playlist_id
+        self.title = self.response["items"][0]["snippet"]["title"]
+        self.url = f'https://www.youtube.com/playlist?list={playlist_id}'
+
