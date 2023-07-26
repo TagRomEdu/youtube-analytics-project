298d922bc7e217d4daf2fc5093c6b30ba9bff6af
TagRomEdu <qwertystyles@gmail.com>
2023-07-26 10:07:42 +0500

    added try/except

--- src/video.py
+++ src/video.py
@@ -5,14 +5,21 @@ class Video(Channel):
 
     def __init__(self, video_id):
         """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
-        request = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id)
-        self.response = request.execute()
+        try:
+            request = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id)
+            self.response = request.execute()
 
-        self.__video_id = video_id
-        self.title = self.response["items"][0]["snippet"]["title"]
-        self.url = f'https://youtu.be/{video_id}'
-        self.like_count = self.response["items"][0]["statistics"]["likeCount"]
-        self.view_count = self.response["items"][0]["statistics"]["viewCount"]
+            self.__video_id = video_id
+            self.title = self.response["items"][0]["snippet"]["title"]
+            self.url = f'https://youtu.be/{video_id}'
+            self.like_count = self.response["items"][0]["statistics"]["likeCount"]
+            self.view_count = self.response["items"][0]["statistics"]["viewCount"]
+        except Exception:
+            self.__video_id = video_id
+            self.title = None
+            self.url = None
+            self.like_count = None
+            self.view_count = None
 
 
 class PLVideo(Video):
